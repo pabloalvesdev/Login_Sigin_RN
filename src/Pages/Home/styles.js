47307ee47fc97c296148e3a50255090c99844505
Pablo Alves <pabloalves.dev@gmail.com>
2021-04-10 14:16:53 -0300

    one

---
+++ src/Pages/Home/styles.js
@@ -0,0 +1,20 @@
+import { StyleSheet } from 'react-native'
+export default HomeStyle = StyleSheet.create({
+    container: {
+        margin: 10,
+        backgroundColor: 'white'
+    },
+    cont: {
+        padding: 15,
+        marginTop: 40,
+        width: '95%',
+        alignSelf: 'center'
+    },
+    title: {
+        fontSize: 20,
+        fontWeight: 'bold'
+    },
+    descrip: {
+        fontSize: 15
+    }
+});
